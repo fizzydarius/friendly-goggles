20c2807fb9d7e7597c6db6cf36d2648d4170e2db
Darius <60950039+yourfavouriteowo@users.noreply.github.com>
2021-11-09 00:02:33 +0000

    Updated main.py with comments
    Added comments on what has been done and what needs to be done, so I can remember it and do it when I get the chance

--- main.py
+++ main.py
@@ -15,11 +15,15 @@ def create_user(user):
         cursor.execute("INSERT INTO database VALUES (:username, :password, :balance)", {"username": user.username, "password": user.password, "balance": user.balance})
         print ("Sucessfull")
 
+
+
 def check_login(username,password):
     x = ("Invalid .")
     cursor.execute("SELECT * FROM database WHERE username=:username And password=:password ", {'username': username, 'password': password })
     info = cursor.fetchall()
 
+    # !!!!! Fix this please darius, come on now !!!!!!
+    
     if info == "[]":
         return x + "ok"
     elif info != "[]":
@@ -36,6 +40,9 @@ user1 = Data("Darius", "password", 30000)
 user2 = Data("steffy", "passwordy", 32000)
 
 
+# turns the checks_login from a list into a string so it's easier to check if if string is empty .
+# if empty, user does not exist ask user to make an account (or password wrong)
+# if it retruns with value then the inputed username/password is correct.
 
 list1 = (check_login(user1.username,user1.password))
 string = ''.join(map(str,list1))
